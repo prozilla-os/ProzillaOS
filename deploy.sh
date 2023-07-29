04cd2c9ed2727ded6003d60c2cceac5600975918
Prozilla <business@prozilla.dev>
2023-07-29 18:37:55 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ DOMAIN=os.prozilla.dev
 COMMIT_MESSAGE="Deployed build to GitHub Pages"
 REPO_URL="https://github.com/Prozilla/Prozilla-OS"
 
+echo Domain: $DOMAIN
+echo Commit message: $COMMIT_MESSAGE
+echo -e "Repository: $REPO_URL\n"
+
 echo $DOMAIN > build/CNAME
 echo Deploying to GitHub Pages...
 gh-pages -d build -m $COMMIT_MESSAGE -r $REPO_URL
