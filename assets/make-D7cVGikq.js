5f6e148aa7f7d896f520b9bfaca2adbc5719d058
Prozilla <business@prozilla.dev>
2024-06-05 20:49:32 +0200

    Deployed

---
+++ assets/make-D7cVGikq.js
@@ -0,0 +1,1 @@
+import{f as r}from"./index-BHE-uA0X.js";import{C as t}from"./command-Clc1HLU6.js";const a=new t().setRequireArgs(!0).setExecute(function(e){if(e[0]==="love")return r(this.name,"*** No rule to make target 'love'. Stop.")});export{a as make};
