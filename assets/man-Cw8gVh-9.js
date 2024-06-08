e5a16667caf9cad3f342febcc5884de8f48dd8cf
Prozilla <business@prozilla.dev>
2024-06-08 21:47:18 +0200

    Deployed

--- assets/man-Cw8gVh-9.js
+++ assets/man-Cw8gVh-9.js
@@ -1,4 +1,4 @@
-import{C as p,f as c,A as a}from"./index-DfxpGFrh.js";import{C as f}from"./command-Clc1HLU6.js";const d=5,$=new f().setRequireArgs(!0).setManual({purpose:"Show system reference manuals",usage:`man [options] page
+import{C as p,f as c,A as a}from"./index-BtJTmIST.js";import{C as f}from"./command-Clc1HLU6.js";const d=5,$=new f().setRequireArgs(!0).setManual({purpose:"Show system reference manuals",usage:`man [options] page
 man -k [options] regexp`,description:`Each page arguments given to man is normally the name of a command.
 The manual page associated with this command is then found and displayed.`,options:{"-k":"Search for manual page using regexp"}}).setExecute(function(u,{options:l}){if(l.includes("k"))return p.search(u[0].toLowerCase()).map(o=>{var i;return(i=o.manual)!=null&&i.purpose?`${o.name} - ${o.manual.purpose}`:o.name}).sort().join(`
 `);const t=u[0].toLowerCase(),m=p.find(t);if(!m)return c(this.name,`${t}: Command not found`);const e=m.manual;if(!e)return c(this.name,`${t}: No manual found`);const r=n=>n.split(`
