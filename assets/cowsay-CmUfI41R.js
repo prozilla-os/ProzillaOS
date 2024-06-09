886efde806483f9b6d621913cfe2fd8bfb91ec65
Prozilla <business@prozilla.dev>
2024-06-09 20:49:26 +0200

    Deployed

--- assets/cowsay-CmUfI41R.js
+++ assets/cowsay-CmUfI41R.js
@@ -1,4 +1,4 @@
-import{M as h}from"./index-C5mDZxnw.js";import{C as u}from"./command-Clc1HLU6.js";const _=`
+import{M as h}from"./index-DcKyF4FJ.js";import{C as u}from"./command-Clc1HLU6.js";const _=`
         \\   ^__^
          \\  (oo)\\_______
             (__)\\       )\\/\\
