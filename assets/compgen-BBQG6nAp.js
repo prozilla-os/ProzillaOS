cfa4e741ddfd0f33ae9b52b1057afe7b87db73a5
Prozilla <business@prozilla.dev>
2024-06-11 21:08:02 +0200

    Deployed build to GitHub Pages

--- assets/compgen-BBQG6nAp.js
+++
@@ -1,2 +0,0 @@
-import{C as o}from"./command-Clc1HLU6.js";import{C as a}from"./index-sfXqOoSv.js";const r=new o().setManual({purpose:"Display a list of all commands"}).setRequireOptions(!0).setExecute(function(s,{options:e}){if(e.includes("c"))return a.COMMANDS.map(n=>n.name).sort().join(`
-`)});export{r as compgen};
--- assets/make-DdYM5K-2.js
+++
@@ -1,1 +0,0 @@
-import{f as r}from"./index-sfXqOoSv.js";import{C as t}from"./command-Clc1HLU6.js";const a=new t().setRequireArgs(!0).setExecute(function(e){if(e[0]==="love")return r(this.name,"*** No rule to make target 'love'. Stop.")});export{a as make};
