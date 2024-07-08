65d884181714077ee319c30e65b42fc3ddb0c7f5
Prozilla <business@prozilla.dev>
2024-07-08 17:13:37 +0200

    Deployed build to GitHub Pages

---
+++ docs/assets/about_index.md.BqVqbrj9.lean.js
@@ -0,0 +1,1 @@
+import{_ as a,c as e,o as t,a2 as r,a3 as o}from"./chunks/framework.CwjcDDsU.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"about/index.md","filePath":"about/index.md"}'),i={name:"about/index.md"},s=r("",7),n=[s];function l(u,p,c,d,h,b){return t(),e("div",null,n)}const f=a(i,[["render",l]]);export{m as __pageData,f as default};
