d4ad2d78b14617489e34b5832d9cba2adf0a8a51
Prozilla <business@prozilla.dev>
2024-07-23 16:26:43 +0200

    Deployed build to GitHub Pages

--- assets/touch-D_kKTiUQ-DxpY7xca.js
+++
@@ -1,3 +0,0 @@
-import{F as r,D as a}from"./index-CjEH4wdM.js";import{o as c}from"./command-nBL1aO7T-CliNWZCP.js";const l=new c().setRequireArgs(!0).setManual({purpose:"Change file timestamps",usage:"touch [options] files",description:`Update the access and modification times of each FILE to the current time.
-
-A file argument that does not exist is created empty.`}).setExecute(function(e,s){const{currentDirectory:t}=s,o=e[0];if(o==="girls\\"&&e[1]==="boo**")return r(this.name,"Cannot touch 'girls boo**': Permission denied");const{name:i,extension:n}=a.splitId(o);return t.findFile(i,n)?{blank:!0}:(t.createFile(i,n),{blank:!0})});export{l as touch};
