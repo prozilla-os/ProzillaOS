a46f6e62810759cf18308d52f26f51f4a864d919
Prozilla <business@prozilla.dev>
2024-10-30 15:46:57 +0100

    Deployed build to GitHub Pages

---
+++ docs/assets/chunks/pixel.-luNSmEG.js
@@ -0,0 +1,1 @@
+const s="/docs/screenshots/skins/mac.png",n="/docs/screenshots/skins/windows95.png",o="/docs/screenshots/skins/minimal.png",c="/docs/screenshots/skins/pixel.png";export{s as _,n as a,o as b,c};
